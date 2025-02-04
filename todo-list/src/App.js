ac263f614d7f2f2b393526e147ae430a5181c25f
Arnatik76 <118046506+Arnatik76@users.noreply.github.com>
2025-02-04 13:52:51 +0500

    Added structure

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -1,10 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <div className="sidebar">
+        <h2>Categories</h2>
+        <div className="input-section">
+          <input type="text" id="categoryInput" placeholder="New category..." />
+          <button id="addCategoryBtn">+</button>
+        </div>
+        <ul id="categoryList">
+          <li className="category-item">Hobbies</li>
+        </ul>
+      </div>
+      <div className="container">
+        <h1>To Do List</h1>
+        <div className="input-section">
+          <input type="text" id="taskInput" placeholder="Add a new task..." />
+          <button id="addTaskBtn">Add</button>
+        </div>
+        <ul id="taskList">
+          <li className="task-item">Make a game</li>
+        </ul>
+      </div>
     </div>
   );
 }
